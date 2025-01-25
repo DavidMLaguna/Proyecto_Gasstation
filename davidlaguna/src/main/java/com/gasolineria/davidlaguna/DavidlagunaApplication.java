136234ba658a6a541571f61f1437d1c703ad4434
jpernia15 <yaonlivvonce287@gmail.com>
2025-01-25 16:14:19 -0400

    carpeta gasolineria creada en spring

---
+++ davidlaguna/src/main/java/com/gasolineria/davidlaguna/DavidlagunaApplication.java
@@ -0,0 +1,13 @@
+package com.gasolineria.davidlaguna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidlagunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidlagunaApplication.class, args);
+	}
+
+}
---
+++ davidlaguna/src/test/java/com/gasolineria/davidlaguna/DavidlagunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasolineria.davidlaguna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavidlagunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
